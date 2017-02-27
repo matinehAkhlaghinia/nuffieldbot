dc0d3a580eafa7082269ae8a512e9d25cf13b9b9
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-02-27 23:17:03 +0000

    added config info

--- messages/index.js
+++ messages/index.js
@@ -11,9 +11,9 @@ var botbuilder_azure = require("botbuilder-azure");
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
+    appId: process.env['62f75b05-11a3-4185-b16e-5cde112a27bf'],
+    appPassword: process.env['Matin1374'],
+    stateEndpoint: process.env['https://NuffieldBot.azurewebsites.net/api/messages?code=CJxOa3qIMcKrhXFsBU/Vu5K9mwD2apsKhJcfQbMgQlrQ0VvethcTFA=='],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
@@ -25,6 +25,40 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', intents);
+
+intents.matches('BookClass', [
+  function (session, args, next) {
+      var className = builder.EntityRecognizer.findEntity(args.entities, 'ClassName');
+      var classDate = builder.EntityRecognizer.findEntity(args.entities, 'ClassDate');
+      var classTime = builder.EntityRecognizer.resolveTime(args.entities);
+      var classInfo = session.dialogData.classInformation = {
+        title: className ? className.entity : null,
+        time:  classTime ? classTime.getTime() : null,
+        date:  classDate ? classDate.entity : null
+      };
+      builder.Prompts.choice(session, "What classes do you want to book?",["Pilates", "Spin", "TRX", "Yoga"]);
+  },
+  function (session, results, next) {
+      session.userData.toBeBooked = results.response.entity;
+      builder.Prompts.text(session, "What date?");
+  },
+  function (session, results, next) {
+      session.userData.date = results.response;
+      builder.Prompts.choice(session, "What time do you want to take your class?",["10-12","2:30-4:30","5:30-7:30"]);
+  },
+  function (session, results, next) {
+      session.userData.time = results.response.entity;
+      builder.Prompts.text(session, "So..you want to book a " + session.userData.toBeBooked + " class, which is on " +
+      session.userData.date + " " + session.userData.time + "?");
+  },
+  function (session, results) {
+      session.userData.confirmation = results.response;
+      if(session.userData.confirmation == "yes") {
+          session.send("Your booking is confirmed!");
+      }
+      session.endDialogWithResult({ response: session.userData });
+  }
+]);
 
 intents.matches('CancelClass', [
   function (session, results) {
@@ -44,52 +78,6 @@ intents.matches('CancelClass', [
   }
 ]);
 
-intents.matches('BookClass', [
-  function (session, results) {
-      //session.userData.task = results.response;
-      builder.Prompts.choice(session, "What classes do you want to book?",["Pilates", "Spin", "TRX", "Yoga"]);
-  },
-  function (session, results) {
-      session.userData.toBeBooked = results.response.entity;
-      builder.Prompts.text(session, "What date?");
-  },
-  function (session, results) {
-      session.userData.date = results.response;
-      builder.Prompts.choice(session, "What time do you want to take your class?",["10-12","2:30-4:30","5:30-7:30"]);
-  },
-  function (session, results) {
-      session.userData.time = results.response.entity;
-      builder.Prompts.text(session, "So..you want to book a " + session.userData.toBeBooked + " class, which is on " +
-      session.userData.date + " " + session.userData.time + "?");
-  },
-  function (session, results) {
-      session.userData.confirmation = results.response;
-      if(session.userData.confirmation == "yes") {
-          session.send("Your booking is confirmed!");
-      }
-      session.endDialogWithResult({ response: session.userData });
-  }
-]);
-
- /*intents.matches('CancelClass', [
-   function (session, results) {
-       //session.userData.task = results.response;
-       builder.Prompts.choice(session, "Which class do you want to cancel?");
-   },
-   function (session, results) {
-       session.userData.toBeCanceled = results.response.entity;
-       builder.Prompts.text(session, "Is this info about the class you want to cancel correct?");
-       builder.Prompts.text(session, session.userData.toBeCanceled);
-   },
-   function (session, results) {
-       session.userData.confirmation = results.response;
-       if(session.userData.confirmation == "yes") {
-           session.send("Your booking is confirmed!");
-       }
-       session.endDialogWithResult({ response: session.userData });
-   }
- ]);*/
-
  intents.matches('ViewClass', [
    function (session, results) {
        //session.userData.task = results.response;
@@ -108,11 +96,6 @@ intents.matches('BookClass', [
        session.endDialogWithResult({ response: session.userData });
    }
  ]);
-
-//intents.matches('ViewClass', (session, args) => { ... });
-//intents.matches('Help', builder.DialogAction.send('Hi! Try asking me things like ...'));
-
-//var bot = new builder.UniversalBot(connector);
 
 if (useEmulator) {
     var restify = require('restify');
