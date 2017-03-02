99b235d2fc475d1f1b5b3d5aed6402fec7c11a3f
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-03-02 20:52:40 +0000

    .

--- messages/index.js
+++ messages/index.js
@@ -40,24 +40,24 @@ bot.dialog('/', intents);
 intents.matches('BookClass', [
   function (session, args, next) {
       console.log("HEY CAME HERE");
-      var className = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassName');
-      var classDate = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassDate');
-      var classTime = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassTime');
-      console.log(className);
-      var classInfo = session.dialogData.classInformation = {
-        title: className ? className.entity : null,
-        time:  classTime ? classTime.entity : null,
-        date:  classDate ? classDate.entity : null
-      };
-      console.log("HEY CAME HERE1");
-      if(!classInfo.title) {
-        console.log("HEY CAME HERE2");
-        builder.Prompts.text(session, "What is the name of the class you want to book?");
-      }
-      else {
-        console.log("HEY CAME HERE3");
-        next();
-      }
+      // var className = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassName');
+      // var classDate = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassDate');
+      // var classTime = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassTime');
+      // console.log(className);
+      // var classInfo = session.dialogData.classInformation = {
+      //   title: className ? className.entity : null,
+      //   time:  classTime ? classTime.entity : null,
+      //   date:  classDate ? classDate.entity : null
+      // };
+      // console.log("HEY CAME HERE1");
+      // if(!classInfo.title) {
+      //   console.log("HEY CAME HERE2");
+      //   builder.Prompts.text(session, "What is the name of the class you want to book?");
+      // }
+      // else {
+      //   console.log("HEY CAME HERE3");
+      //   next();
+      // }
       //builder.Prompts.choice(session, "What classes do you want to book?",["Pilates", "Spin", "TRX", "Yoga"]);
   },
   function (session, results, next) {
