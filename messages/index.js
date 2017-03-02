75037fe498aa6ee38141e51d31892fac8be582d7
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-03-02 20:50:46 +0000

    commented out unneeded stuff

--- messages/index.js
+++ messages/index.js
@@ -20,14 +20,13 @@ var botbuilder_azure = require("botbuilder-azure");
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
-// var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-//     appId: process.env['62f75b05-11a3-4185-b16e-5cde112a27bf'],
-//     appPassword: process.env['gEExYArFEnOz42Po3Rm2NEL'],
-//     stateEndpoint: process.env['https://NuffieldBot.azurewebsites.net/api/messages?code=CJxOa3qIMcKrhXFsBU/Vu5K9mwD2apsKhJcfQbMgQlrQ0VvethcTFA=='],
-//     openIdMetadata: process.env['BotOpenIdMetadata']
-// });
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    // appId: process.env['62f75b05-11a3-4185-b16e-5cde112a27bf'],
+    // appPassword: process.env['gEExYArFEnOz42Po3Rm2NEL'],
+    // stateEndpoint: process.env['https://NuffieldBot.azurewebsites.net/api/messages?code=CJxOa3qIMcKrhXFsBU/Vu5K9mwD2apsKhJcfQbMgQlrQ0VvethcTFA=='],
+    // openIdMetadata: process.env['BotOpenIdMetadata']
+});
 
-var connector = new builder.ConsoleConnector().listen();
 
 const LuisModelUrl = 'https://api.projectoxford.ai/luis/v1/application?id=077297b8-f0f0-496a-8b6a-362eb36ef53f&subscription-key=4bfee3fdd12e428ba1424426479fc04a';
 
@@ -41,9 +40,10 @@ bot.dialog('/', intents);
 intents.matches('BookClass', [
   function (session, args, next) {
       console.log("HEY CAME HERE");
-      var className = builder.EntityRecognizer.findEntity(args.entities, 'ClassName');
-      var classDate = builder.EntityRecognizer.findEntity(args.entities, 'ClassDate');
-      var classTime = builder.EntityRecognizer.findEntity(args.entities, 'ClassTime');
+      var className = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassName');
+      var classDate = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassDate');
+      var classTime = builder.EntityRecognizer.findEntity(args.intent.entities, 'ClassTime');
+      console.log(className);
       var classInfo = session.dialogData.classInformation = {
         title: className ? className.entity : null,
         time:  classTime ? classTime.entity : null,
