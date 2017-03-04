242e342a6df4f14f040f6496946cdb98409330cf
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-03-04 00:25:27 +0000

    files replaced

--- messages/index.js
+++ messages/index.js
@@ -4,41 +4,28 @@ For a complete walkthrough of creating this type of bot see the article at
 https://docs.botframework.com/en-us/node/builder/chat/dialogs/#waterfall
 -----------------------------------------------------------------------------*/
 "use strict";
-
-
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 
 var useEmulator = (process.env.NODE_ENV == 'development');
-//useEmulator = true;
-
-// var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-//      appId: process.env['62f75b05-11a3-4185-b16e-5cde112a27bf'],
-//      appPassword: process.env['gEExYArFEnOz42Po3Rm2NEL'],
-//      stateEndpoint: process.env['https://NuffieldBot.azurewebsites.net/api/messages?code=CJxOa3qIMcKrhXFsBU/Vu5K9mwD2apsKhJcfQbMgQlrQ0VvethcTFA=='],
-//      openIdMetadata: process.env['BotOpenIdMetadata']
-// });
-
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['c3154986-1673-4f56-b9f9-965cd361d3cd'],
-    appPassword: process.env['FCckmnn3QCQWB6MRkbxxfgR'],
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
     stateEndpoint: process.env['BotStateEndpoint'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
-//const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/077297b8-f0f0-496a-8b6a-362eb36ef53f?subscription-key=4bfee3fdd12e428ba1424426479fc04a';
 const LuisModelUrl = 'https://api.projectoxford.ai/luis/v1/application?id=077297b8-f0f0-496a-8b6a-362eb36ef53f&subscription-key=4bfee3fdd12e428ba1424426479fc04a';
 
-// Main dialog with LUIS
+
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', intents);
 
-
- intents.matches('BookClass', [
+intents.matches('BookClass', [
    function (session, args, next) {
 
         var className = builder.EntityRecognizer.findEntity(args.entities, 'ClassName');
@@ -118,50 +105,14 @@ bot.dialog('/', intents);
   }*/
 ]);
 
-// intents.matches('CancelClass', [
-//   function (session, results) {
-//       //session.userData.task = results.response;
-//       builder.Prompts.text(session, "Which class do you want to cancel?");
-//   },
-//   function (session, results) {
-//       session.userData.toBeCanceled = results.response.entity;
-//       builder.Prompts.text(session, "Is this info about the class you want to cancel correct?"+ ": "+ session.userData.toBeCanceled);
-//   },
-//   function (session, results) {
-//       session.userData.confirmation = results.response;
-//       if(session.userData.confirmation == "yes") {
-//           session.send("Your booking is confirmed!");
-//       }
-//       session.endDialogWithResult({ response: session.userData });
-//   }
-// ]);
-//
-//  intents.matches('ViewClass', [
-//    function (session, results) {
-//        //session.userData.task = results.response;
-//        builder.Prompts.text(session, "For what date do you want to see the available classes?");
-//    },
-//    function (session, results) {
-//        session.userData.date = results.response.entity;
-//        builder.Prompts.text(session, "These are the available classes: Pilates, Spin, TRX, Yoga");
-//    },
-//    function (session, results) {
-//        session.userData.confirmation = results.response;
-//        builder.Prompts.text(session, "Is there anything else you want to do?");
-//       //  if(session.userData.confirmation == "yes") {
-//       //      session.send("Your booking is confirmed!");
-//       //  }
-//        session.endDialogWithResult({ response: session.userData });
-//    }
-//  ]);
-//
-// if (true) {
-//     var restify = require('restify');
-//     var server = restify.createServer();
-//     server.listen(3978, function() {
-//         console.log('test bot endpont at http://localhost:3978/api/messages');
-//     });
-//     server.post('/api/messages', connector.listen());
-// } else {
-//     module.exports = { default: connector.listen() }
-// }
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    module.exports = { default: connector.listen() }
+}
