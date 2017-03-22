f69df3cd2ec1d352196b6c4903a2399b1ae98d1f
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-03-22 23:34:42 +0000

    restify module added

--- messages/index.js
+++ messages/index.js
@@ -7,6 +7,7 @@ https://docs.botframework.com/en-us/node/builder/chat/dialogs/#waterfall
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var request = require('request');
+var restify = require('restify');
 var server = restify.createServer();
 
 var useEmulator = (process.env.NODE_ENV == 'development');
@@ -385,7 +386,6 @@ server.post('/callback', function(req, res){
 })
 
 if (useEmulator) {
-    var restify = require('restify');
     server.listen(3978, function() {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
