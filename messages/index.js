39b3b14e064f109d7bfa2f449b5342f2768d9a6e
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-03-23 21:52:05 +0000

    emulator set to false

--- messages/index.js
+++ messages/index.js
@@ -10,7 +10,7 @@ var request = require('request');
 var restify = require('restify');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
-useEmulator = true;
+//useEmulator = true;
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
